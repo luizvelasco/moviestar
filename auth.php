08ece8532c8acd62b5a30231491e5fbe36521c33
luizvelasco <luizvelasco@gmail.com>
2023-06-18 12:25:03 -0300

    templates

--- auth.php
+++ auth.php
@@ -2,7 +2,7 @@
     require_once("templates/header.php");
 ?>
     <div id="main-container" class="container-fluid">
-        <h1>Corpo do site</h1>
+        <h1>Autenticação</h1>
     </div>
 <?php
     require_once("templates/footer.php");
