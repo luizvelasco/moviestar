5ed7d46879640007cb7145b6005aa2c0ecff1cbd
Luiz Velasco <luizvelasco@gmail.com>
2023-06-19 14:01:39 -0300

    teste

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once("templates/header.php");
 ?>
     <div id="main-container" class="container-fluid">
-        <h1>Corpo do site</h1>
+        <h1>Corpo do site 2</h1>
     </div>
 <?php
     require_once("templates/footer.php");
