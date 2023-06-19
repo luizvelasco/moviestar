94033c597642e0d13bcea14bd39e64bb1b8880a7
Luiz Velasco <luizvelasco@gmail.com>
2023-06-19 18:17:54 -0300

    validações

--- auth_process.php
+++ auth_process.php
@@ -8,6 +8,8 @@ require_once("db.php");
 
 $message = new Message($BASE_URL);
 
+$userDao = new UserDAO($conn, $BASE_URL);
+
 //Resgata o tipo de formulário
 $type = filter_input(INPUT_POST, "type"); //Resgatar os inputs já livres de qqer dado inserido de forma maliciosa (está vindo de um POST e seu nome é type)
 
@@ -23,6 +25,28 @@ if ($type === "register") {
     // Verificação de dados mínimos
 
     if ($name && $lastname && $email && $password) {
+
+        //Verificar se as senhas batem
+        if ($password === $confirmpassword){
+
+            //Verificar se o e-mail já está cadastrado no sistema
+            if ($userDao->findByEmail($email) === false) {
+
+                echo "Nenhum usuário foi encontrado";
+
+            } else {
+
+                //Enviar uma msg de erro, usuário já existe
+                $message->setMessage("Usuário já cadastrado, tente outro e-mail.", "error", "back");
+
+            }
+
+        } else {
+
+            //Enviar uma msg de erro, de senhas diferentes
+            $message->setMessage("As senhas não são iguais.", "error", "back");
+
+        }
 
     } else {
 
--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -52,6 +52,31 @@ class UserDAO implements USerDAOInterface{
 
     public function findByEmail ($email) {
 
+        //Verifica se foi passado algo no e-mail
+        if ($email != "") {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = :email");
+
+            $stmt->bindParam(":email", $email);
+
+            $stmt->execute();
+
+            //Testa se retornou registros
+            if ($stmt->rowCount() > 0) {
+
+                //Fetch() só pega um usuário
+                $data = $stmt->fetch();
+                //Retorna o objeto montado do usuário
+                $user = $this->buildUser($data);
+                return $user;
+
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+
     }
 
     public function findById ($id) {
