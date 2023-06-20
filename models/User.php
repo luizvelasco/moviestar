17dc3f2b834eab68e78d7c4d1d8fd4b20f919df7
Luiz Velasco <luizvelasco@gmail.com>
2023-06-20 20:06:10 -0300

    layout alterar senha

--- models/User.php
+++ models/User.php
@@ -11,6 +11,10 @@ class User {
     public $bio;
     public $token;
 
+    public function getFullName($user){
+        return $user->name . " " . $user->lastname;
+    }
+
     public function generateToken(){
         return bin2hex(random_bytes(50));
     }
--- templates/header.php
+++ templates/header.php
@@ -54,7 +54,7 @@
                     <?php if ($userData): ?>
                         <li class="nav-item">
                             <a href="<?= $BASE_URL; ?>newmovie.php" class="nav-link">
-                                <i class="far fa-plus-square"></i>Incluir Filme
+                                <i class="far fa-plus-square"></i> Incluir Filme
                             </a>
                         </li>
                         <li class="nav-item">
