b471711683efc9588850853779753e90fcfec651
luizvelasco <luizvelasco@gmail.com>
2023-06-27 09:13:31 -0300

    delete

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -151,6 +151,15 @@ class MovieDAO implements MovieDAOInterface
 
     }
     public function destroy($id) {
+
+        $stmt = $this->conn->prepare("DELETE FROM movies WHERE id = :id");
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        //Mensagem de sucesso por remover filme
+        $this->message->setMessage("Filme excluído com sucesso", "success", "dashboard.php");
         
     }
 
