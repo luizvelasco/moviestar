05c472356ec8141c7c4e3c4edfd16df97be8a22f
luizvelasco <luizvelasco@gmail.com>
2023-07-02 18:04:46 -0300

    conclusão

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -136,6 +136,26 @@ class MovieDAO implements MovieDAOInterface
     }
     public function findByTitle($title) {
 
+        $movies = [];
+
+        $stmt = $this->conn->prepare("SELECT * FROM movies WHERE title LIKE :title");
+
+        $stmt->bindValue(":title", '%' . $title . '%');
+        
+        $stmt->execute();
+        
+        if ($stmt->rowCount() > 0) {
+
+            $moviesArray = $stmt->fetchAll();
+
+            foreach ($moviesArray as $movie ) {
+                $movies[] = $this->buildMovie($movie);
+            }
+
+        }
+
+        return $movies;
+
     }
     public function create(Movie $movie) {
 
