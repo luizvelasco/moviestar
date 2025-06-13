69bb5cf1cfa5fdae81722552f4ce3706e621110d
Luiz Velasco <luizvelasco@gmail.com>
2025-06-13 19:15:35 -0300

    estrutura do login

--- auth.php
+++ auth.php
@@ -3,7 +3,52 @@
 ?>
 
     <div id="main-container" class="container-fluid">
-        <h1>Autenticação</h1>
+        <div class="col-md-12">
+            <div class="row" id = "auth-row">
+                <div class="col-md-4" id="login-container">
+                    <h2>Entrar</h2>
+                    <form action="" method="post">
+                        <input type="hidden"  name="type" name="login">
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="text" class="form-control" id="email" name="email"  placeholder="Digite seu e-mail">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Senha</label>
+                            <input type="password" class="form-control" id="password" name="password"  placeholder="Digite sua senha">
+                        </div>
+                        <input type="submit" class="btn card-btn" value="Entrar">
+                    </form>
+                </div>
+                <div class="col-md-4" id="login-container">
+                    <h2>Criar Conta</h2>
+                     <form action="" method="post">
+                        <input type="hidden"  name="type" name="register">
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" class="form-control" id="email" name="email"  placeholder="Digite seu e-mail">
+                        </div>
+                        <div class="form-group">
+                            <label for="nome">Nome</label>
+                            <input type="text" class="form-control" id="name" name="name"  placeholder="Digite seu nome">
+                        </div>
+                        <div class="form-group">
+                            <label for="nome">Sobrenome</label>
+                            <input type="text" class="form-control" id="lastname" name="lastname"  placeholder="Digite seu sobrenome">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Senha</label>
+                            <input type="password" class="form-control" id="password" name="password"  placeholder="Digite sua senha">
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmpassword">Confirmação de senha</label>
+                            <input type="password" class="form-control" id="confirmpassword" name="confirmpassword"  placeholder="Confirme sua senha">
+                        </div>
+                        <input type="submit" class="btn card-btn" value="Registrar">
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php
