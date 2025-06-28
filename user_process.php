fab1272931e8f22073f7ba69e87350ea6e59c5f4
luizvelasco <luizvelasco@gmail.com>
2025-06-28 19:41:29 -0300

    upload de imagem do usuário

--- user_process.php
+++ user_process.php
@@ -34,6 +34,43 @@
         $userData->email = $email;
         $userData->bio = $bio;
 
+        // Upload da imagem
+        if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+      
+        $image = $_FILES["image"];
+        $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+        $jpgArray = ["image/jpeg", "image/jpg"];
+
+        // Checagem de tipo de imagem
+        if(in_array($image["type"], $imageTypes)) {
+
+            // Checar se jpg
+            if(in_array($image, $jpgArray)) {
+
+            $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+
+            // Imagem é png
+            } else {
+
+            $imageFile = imagecreatefrompng($image["tmp_name"]);
+
+            }
+
+            $imageName = $user->imageGenerateName();
+
+            imagejpeg($imageFile, "./img/users/" . $imageName, 100);
+
+            $userData->image = $imageName;
+
+        } else {
+            
+            $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
+            exit;
+
+        }
+
+    } 
+
         $userDao->update($userData);
 
     // Atualizar a senha do usuário
