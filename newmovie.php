317c0a523e4d3287cc4e32168b70fca0327d9944
luizvelasco <luizvelasco@gmail.com>
2025-06-29 16:44:08 -0300

    model movie

--- newmovie.php
+++ newmovie.php
@@ -12,7 +12,7 @@
 ?>
 
     <div id="main-container" class="container-fluid">
-        <div class="offset-md-4 col-md-4 new-movie-cointainer">
+        <div class="offset-md-4 col-md-4 new-movie-container">
             <h1 class="page-title">Adicionar Filme</h1>
             <p class="page-description">Adicione sua crítica e compartilhe com o mundo!</p>
             <form action="<?= $BASE_URL ?>movie_process.php" id="add-movie-form" method="post" enctype="multipart/form-data">
