40a2305696f7ea34e0402107995319638b04469e
luizvelasco <luizvelasco@gmail.com>
2025-06-29 15:52:14 -0300

    formulario novo filme

---
+++ newmovie.php
@@ -0,0 +1,57 @@
+<?php
+    require_once("templates/header.php");
+
+    // Verifica se usuário está autenticado
+    require_once("models/User.php");
+    require_once("dao/UserDAO.php");
+
+    $user = new User();
+    $userDao = new UserDAO($conn, $BASE_URL);
+
+    $userData = $userDao->verifyToken(true);
+?>
+
+    <div id="main-container" class="container-fluid">
+        <div class="offset-md-4 col-md-4 new-movie-cointainer">
+            <h1 class="page-title">Adicionar Filme</h1>
+            <p class="page-description">Adicione sua crítica e compartilhe com o mundo!</p>
+            <form action="<?= $BASE_URL ?>movie_process.php" id="add-movie-form" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="type" value="create">
+                <div class="form-group">
+                    <label for="title">Título</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Digite o título do seu filme">
+                </div>
+                <div class="form-group">
+                    <label for="image">Imagem</label>
+                    <input type="file" class="form-control-file" name="image" id="image">
+                </div>
+                <div class="form-group">
+                    <label for="length">Duração</label>
+                    <input type="text" class="form-control" id="length" name="length" placeholder="Digite a duração do filme">
+                </div>
+                <div class="form-group">
+                    <label for="category">Categoria</label>
+                    <select class="form-control" name="category" id="category">
+                        <option value="">Selecione</option>
+                        <option value="Ação">Ação</option>
+                        <option value="Drama">Drama</option>
+                        <option value="Comédia">Comédia</option>
+                        <option value="Romance">Romance</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="trailer">Trailer</label>
+                    <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Digite a duração do filme">
+                </div>
+                <div class="form-group">
+                    <label for="description">Descrição</label>
+                    <textarea class="form-control" name="description" id="description" rows="5" placeholder="Descreva o filme"></textarea>
+                </div>
+                <input type="submit" class="btn card-btn" value="Adicionar filme">
+            </form>
+        </div>
+    </div>
+    
+<?php
+    require_once("templates/footer.php");
+?>
